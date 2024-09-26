d29015538953b00af831290a06a50cedb3c9d3c8
atishe <you@example.com>
2024-09-26 14:30:10 +0530

    backend update

--- controller/public.controller.js
+++ controller/public.controller.js
@@ -4,9 +4,17 @@ const Carousel = require("../models/Carousel")
 
 exports.fetchProjects = asyncHandler(async (req, res) => {
     const result = await Projects.find()
-    res.json({message:"Project Fetch Success...!", result})
+    res.status(200).json({ message: "Project Fetch Success...!", result })
 })
+
 exports.getAllCarousel = asyncHandler(async (req, res) => {
     const result = await Carousel.find()
     res.status(200).json({ message: "blog fetch success", result })
+})
+exports.getProjectDetails = asyncHandler(async (req, res) => {
+    const result = await Projects.findById(req.params.id)
+    if (!result) {
+        return res.status(404).json({ message: "Project not found" })
+    }
+    res.status(200).json({ message: "Fetch project details success", result })
 })
